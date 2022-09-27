97b578d2765624fbb715166ad31a43008ac3aed6
Anne McCorkle <annefmccorkle@gmail.com>
2022-09-27 11:54:58 -0600

    reworking app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+//createHttpLink would be to have a graphql api endpoint
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//if using the graphql api endpoint, but using the inMemoryCache connection instead
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  //using the browser cache
+  cache: new InMemoryCache(),
+})
+
+//
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route 
+              path='/' 
+              element={<SearchBooks />} 
+            />
+            <Route 
+              path='/saved' 
+              element={<SavedBooks />} 
+            />
+            <Route 
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
